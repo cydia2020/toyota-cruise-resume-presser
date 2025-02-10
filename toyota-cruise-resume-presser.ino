0b2df685676c59f74e1482cc0ca992b4ec42eaed
Irene <12470297+cydia2020@users.noreply.github.com>
2025-02-10 13:46:55 +1100

    vastly simplify code

--- toyota-cruise-resume-presser.ino
+++ toyota-cruise-resume-presser.ino
@@ -1,53 +1,45 @@
 #include <SPI.h>
-#include "mcp2515_can.h"
-#include "mcp_can.h"
-#define CAN_2515
+#include <mcp_canbus.h>
 
 // Define CAN bus pins
-const int SPI_CS_PIN = 10;
-const int CAN_INTERRUPT_PIN = 2;
+const int SPI_CS_PIN = 9;
+const int CAN_INTERRUPT_PIN = 11;
 
-bool canMessageAvailable = false;
+// Define relay control pin
+const int RESUME_BUTTON_TRANSISTOR_PIN = 10;
+
+volatile bool canMessageAvailable = false;
 unsigned long lastRxId;
 byte lastRxBuf[8];
-byte lastDlc;
 
-mcp2515_can CAN0(SPI_CS_PIN);
+MCP_CAN CAN0(SPI_CS_PIN);
 
-#define ACC_CONTROL 0x343
-#define PCM_CRUISE 0x1D2
-#define PCM_CRUISE_2 0x1D3
+#define PCM_CRUISE_SM 0x399
 
-// Define variables to remember the last state of the relevant IDs
-bool resumeReady = false;
-bool transistorPowered = false;
-bool pcmStandstill = false;
-bool brakePressed = false;
+// Define state machine
+enum TransistorState { TRANSISTOR_OFF,
+                       TRANSISTOR_ON };
+TransistorState transistorState = TRANSISTOR_OFF;
 
-unsigned long transistorOnTime = 0;
-const unsigned long transistorDuration = 20;
-
-const int BUTTON_TRANSISTOR_PIN = A1;
+// Variables tracking CAN data
+bool pcmResumeReady = false;
 
 void setup() {
-  pinMode(BUTTON_TRANSISTOR_PIN, OUTPUT);
-  digitalWrite(BUTTON_TRANSISTOR_PIN, LOW);
-  attachInterrupt(digitalPinToInterrupt(CAN_INTERRUPT_PIN), MCP2515_ISR, FALLING);
+  pinMode(RESUME_BUTTON_TRANSISTOR_PIN, OUTPUT);
+  digitalWrite(RESUME_BUTTON_TRANSISTOR_PIN, LOW);
+  pinMode(CAN_INTERRUPT_PIN, INPUT_PULLUP);
 
-  bool resumeReady = false;
-  bool transistorPowered = false;
-  bool pcmStandstill = false;
-  bool brakePressed = false;
+  attachInterrupt(digitalPinToInterrupt(CAN_INTERRUPT_PIN), MCP2515_ISR, FALLING);
 
   if (CAN0.begin(CAN_500KBPS) == CAN_OK) {
     CAN0.init_Mask(0, 0, 0x7FF);
-    CAN0.init_Filt(0, 0, 0x343);
-    CAN0.init_Filt(1, 0, 0x1D2);
+    CAN0.init_Filt(0, 0, 0x399);
+    CAN0.init_Filt(1, 0, 0x399);
     CAN0.init_Mask(1, 0, 0x7FF);
-    CAN0.init_Filt(2, 0, 0x1D3);
-    CAN0.init_Filt(3, 0, 0x343);
-    CAN0.init_Filt(4, 0, 0x1D2);
-    CAN0.init_Filt(5, 0, 0x1D3);
+    CAN0.init_Filt(2, 0, 0x399);
+    CAN0.init_Filt(3, 0, 0x399);
+    CAN0.init_Filt(4, 0, 0x399);
+    CAN0.init_Filt(5, 0, 0x399);
   }
 }
 
@@ -56,43 +48,34 @@ void MCP2515_ISR() {
 }
 
 void loop() {
-  // Check if a CAN message is available
-  //  if (CAN0.checkReceive() == CAN_MSGAVAIL) { when not using interrupts, you really should be using interrupts
-  // interrupt flag
   if (canMessageAvailable) {
-    // reset flag
     canMessageAvailable = false;
-    // now read the CAN message
-    CAN0.readMsgBufID(&lastRxId, &lastDlc, lastRxBuf);
-    processCanMessage();  // Process the message
+    CAN0.readMsgBufID(&lastRxId, nullptr, lastRxBuf);
+    processCanMessage();
   }
-
-  // Turn off the transistor after transistorDuration
-  if (transistorPowered && (millis() - transistorOnTime >= transistorDuration)) {
-    PORTC &= ~(1 << PC1);
-    transistorPowered = false;  // Reset the flag
-  }
+  updateTransistorState();
 }
 
 void processCanMessage() {
-  // Process the CAN message from the last CAN read
-  if (lastDlc == 8 && lastRxId == ACC_CONTROL) {
-    resumeReady = bitRead(lastRxBuf[3], 7);  // Read resume ready state
-  }
+  switch (lastRxId) {
 
-  if (lastDlc == 8 && lastRxId == PCM_CRUISE) {
-    pcmStandstill = ((lastRxBuf[6] & 0xF0) >> 4 == 7 || (lastRxBuf[6] & 0xF0) >> 4 == 11);  // Check CRUISE_STATE
+    case PCM_CRUISE_SM:
+      pcmResumeReady = (lastRxBuf[1] & 0x0F) == 10;
+      break;
   }
+}
 
-  // get brake pressed from brake_module
-  if (lastDlc == 8 && lastRxId == PCM_CRUISE_2) {
-    brakePressed = bitRead(lastRxBuf[0], 3);
-  }
+void updateTransistorState() {
+  static bool lastTransistorState = LOW;
 
-  // Power the transistor briefly if both conditions are true and it's not already powered
-  if (resumeReady && pcmStandstill && !brakePressed) {
-    PORTC |= (1 << PC1);          // Power the transistor
-    transistorOnTime = millis();  // Record the time when the transistor was turned on
-    transistorPowered = true;     // Set the flag indicating the transistor is powered
+  transistorState = pcmResumeReady ? TRANSISTOR_ON : TRANSISTOR_OFF;
+
+  if (transistorState != lastTransistorState) {
+    if (transistorState == TRANSISTOR_ON) {
+      digitalWrite(RESUME_BUTTON_TRANSISTOR_PIN, HIGH);
+    } else {
+      digitalWrite(RESUME_BUTTON_TRANSISTOR_PIN, LOW);
+    }
+    lastTransistorState = transistorState;
   }
 }
